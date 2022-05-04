98df66d0d90e1b3a9c8375c7fdb2fff06b811fb8
s <s@salva.io>
2022-05-04 01:01:24 -0300

    feat: update Candidate type.

--- src/types/Candidate.ts
+++ src/types/Candidate.ts
@@ -1,3 +1,12 @@
 export type Candidate = {
+  id?: string;
+  birth: string;
+  career_experience: string;
+  career_title: string;
+  email: string;
+  last: string;
+  linkedin: string;
   name: string;
-}
+  phone: string;
+  skills?: string;
+};
